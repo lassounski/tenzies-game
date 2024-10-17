15486c4e81f1e5e0fad7c941c5c826e08e1cfcc5
Kirill Lassounski <lassounski@gmail.com>
2024-10-17 09:42:49 +0200

    adjusting syle and refactoring into grid container to show the dices

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -4,7 +4,7 @@ export default function Instructions() {
     return (
         <div className="instructions--container">
             <h1 className="instructions--title">Tenzies</h1>
-            <p className="instructions--text">Roll until all ice are the same. 
+            <p className="instructions--text">Roll until all dice are the same. 
                 Click each die to freeze it at its current value between the rolls.</p>
         </div>
     )
