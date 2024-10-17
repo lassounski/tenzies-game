e2868e4cf17a8c3a50bf405e6ce1e6dc5f35aee4
Kirill Lassounski <lassounski@gmail.com>
2024-10-17 09:08:44 +0200

    Creating the main game container

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Instructions from "./components/Instructions"
+
+export default function App() {
+  return (
+      <main>
+        <div className="game--container">
+          <Instructions />
+        </div>
+      </main>
+  )
+}
---
+++ src/components/Instructions.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Instructions() {
+    return (
+        <div className="instructions--container">
+            <h1 className="instructions--title">Tenzies</h1>
+            <p className="instructions--text">Roll until all ice are the same. 
+                Click each die to freeze it at its current value between the rolls.</p>
+        </div>
+    )
+}
