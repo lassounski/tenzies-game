5a4f5233015120cfb9ae6e539a9a3ac4ab21105a
Kirill Lassounski <lassounski@gmail.com>
2024-10-17 17:00:12 +0200

    making dices look like actual dices

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,17 +1,40 @@
 import React from "react"
 
+import '../dice.css';
+
 export default function ({ props, holdFunction } ) {
-    
+    const matrix = Array(3).fill().map(() => Array(3).fill(false));
+
+    // Define the positions for each die number
+    const positions = {
+        1: [[1, 1]],                       // Center
+        2: [[0, 0], [2, 2]],               // Top-left, Bottom-right
+        3: [[0, 0], [1, 1], [2, 2]],       // Top-left, Center, Bottom-right
+        4: [[0, 0], [0, 2], [2, 0], [2, 2]], // Four corners
+        5: [[0, 0], [0, 2], [1, 1], [2, 0], [2, 2]], // Four corners + Center
+        6: [[0, 0], [0, 2], [1, 0], [1, 2], [2, 0], [2, 2]] // Top & Bottom rows + middle columns
+    };
+
+    // Set true in the matrix for the positions that correspond to the die value from props
+    positions[props.value].forEach(([row, col]) => {
+        matrix[row][col] = true;
+    });
+
     const handleClick = () => {
         holdFunction(props.id)
     }
 
     return (
         <div
-            className={`dice--container ${props.isHeld && 'dice--selected'}`}
+            key={props.key}
+            className={`dice ${props.isHeld && 'dice--selected'}`}
             onClick={handleClick}
         >
-            <span>{props.value}</span>
+            {matrix.map(row => (
+                row.map((cell, colIndex) => (
+                    <div key={colIndex} className={cell && "dot"}/>
+                ))
+            ))}
         </div>
     )
 }
