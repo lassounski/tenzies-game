8e52d3e2161104e419a1289fa7d648dfcc1de9a4
Kirill Lassounski <lassounski@gmail.com>
2024-10-17 09:34:49 +0200

    adding some dices

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import React from "react"
 
 import Instructions from "./components/Instructions"
+import Die from "./components/Die"
 
 export default function App() {
   return (
       <main>
         <div className="game--container">
           <Instructions />
+          <div className="dies--container">
+            <Die value="1"/>
+            <Die value="3"/>
+            <Die value="1"/>
+            <Die value="3"/>
+            <Die value="1"/>
+
+            <Die value="5"/>
+            <Die value="1"/>
+            <Die value="2"/>
+            <Die value="4"/>
+            <Die value="6"/>
+          </div>
         </div>
       </main>
   )
